deaa00a22fbe060c01e39d30156a5e223432fe1e
Brock Haugen <brock.a.haugen@gmail.com>
2015-11-05 15:30:03 -0800

    feed and item CRUD apis

--- server/alchemy/base_models.py
+++ server/alchemy/base_models.py
@@ -6,6 +6,10 @@ class User(Base):
     __tablename__ = 'users'
     wwuid = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    mask_photo = Column(String(250))
+    subscriptions = Column(String(2500))
+    def to_json(self):
+        return {'wwuid': str(self.wwuid), 'name': str(self.name), 'mask_photo': str(self.mask_photo), 'subscriptions': str(self.subscriptions)}
 
 class Feed(Base):
     __tablename__ = 'feeds'
@@ -13,7 +17,10 @@ class Feed(Base):
     name = Column(String(250), nullable=False)
     description = Column(String(250))
     owner = Column(Integer, ForeignKey("users.wwuid"), nullable=False)
+    administrators = Column(String(1000))
     private = Column(Boolean, default=True)
+    def to_json(self):
+        return {'id': str(self.id), 'name': str(self.name), 'description': str(self.description), 'owner': str(self.owner), 'administrators': str(self.administrators), 'private': str(self.private)}
 
 class Item(Base):
     __tablename__ = 'items'
@@ -21,7 +28,9 @@ class Item(Base):
     feed_id = Column(Integer, ForeignKey("feeds.id"), nullable=False)
     name = Column(String(250), nullable=False)
     description = Column(String(250))
-    start_time = Column(DateTime, nullable=False)
-    end_time = Column(DateTime, nullable=False)
+    start_time = Column(String(250), nullable=False)
+    end_time = Column(String(250), nullable=False)
     creator = Column(Integer, ForeignKey("users.wwuid"), nullable=False)
     updated_at = Column(DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+    def to_json(self):
+        return {'id': str(self.id), 'feed_id': str(self.feed_id), 'name': str(self.name), 'description': str(self.description), 'start_time': str(self.start_time), 'end_time': str(self.end_time), 'creator': str(self.creator), 'updated_at': str(self.updated_at)}
--- server/alchemy/feed_handlers.py
+++ server/alchemy/feed_handlers.py
@@ -8,15 +8,120 @@ from alchemy.base_handlers import BaseHandler
 
 logger = logging.getLogger("pyserver")
 
-# CRUD feeds
-# class FeedHandler(BaseHandler):
-#     def get(self):
-#
-#     def post(self):
-#
-#     def put(self):
-#
-#     def delete(self):
+class FeedHandler(BaseHandler):
+    def get(self, id):
+        feed = query_by_id(Feed, id)
+        if not feed:
+            return self.write({'error':'feed does not exist'})
+        self.write({'feed':feed.to_json()})
 
-# CRUD items
-# get planner items
+    @tornado.web.authenticated
+    def put(self):
+        name = self.get_argument("name", None)
+        user = self.current_user
+        if not name:
+            return self.write({'error':'you must give us a name'})
+        administrators = self.get_argument("administrators", None)
+        feed = Feed(name=name,owner=user.wwuid,administrators=administrators)
+        feed = addOrUpdate(feed)
+        self.write({'feed': feed.to_json()})
+
+    @tornado.web.authenticated
+    def post(self, id):
+        name = self.get_argument("name", None)
+        user = self.current_user
+        if not name:
+            return self.write({'error':'you must give us a name'})
+        feed = query_by_id(Feed, id)
+        if not feed:
+            return self.write({'error':'feed does not exist'})
+        if feed.owner != user.wwuid and user.wwuid not in feed.administrators.split(","):
+            return self.write({'error':'insufficient permissions'})
+        administrators = self.get_argument("administrators", None)
+        feed.name = name
+        feed.administrators = administrators
+        feed = addOrUpdate(feed)
+        self.write({'feed': feed.to_json()})
+
+    @tornado.web.authenticated
+    def delete(self, id):
+        user = self.current_user
+        feed = query_by_id(Feed, id)
+        if not feed:
+            return self.write({'error':'feed does not exist'})
+        if feed.owner != user.wwuid:
+            return self.write({'error':'insufficient permissions'})
+        for item in query_by_field(Item, "feed_id", id):
+            delete_thing(item)
+        delete_thing(feed)
+        self.write(json.dumps("success"))
+
+
+class ItemHandler(BaseHandler):
+    def get(self, id):
+        item = query_by_id(Item, id)
+        if not item:
+            return self.write({'error':'item does not exist'})
+        self.write({'item':item.to_json()})
+
+    @tornado.web.authenticated
+    def put(self):
+        user = self.current_user
+        feed_id = self.get_argument("feed_id", None)
+        feed = query_by_id(Feed, feed_id)
+        if not feed:
+            return self.write({'error':'feed does not exist'})
+        if feed.owner != user.wwuid and user.wwuid not in feed.administrators.split(","):
+            return self.write({'error':'insufficient permissions'})
+        name = self.get_argument("name", None)
+        start_time = self.get_argument("start_time", None)
+        end_time = self.get_argument("end_time", None)
+        user = self.current_user
+        if not name or not start_time or not end_time:
+            return self.write({'error':'you must give us a name and a start_time and an end_time'})
+        description = self.get_argument("description", None)
+        item = Item(feed_id=feed_id,name=name,creator=user.wwuid,description=description,start_time=start_time,end_time=end_time)
+        item = addOrUpdate(item)
+        self.write({'item': item.to_json()})
+
+    @tornado.web.authenticated
+    def post(self, id):
+        user = self.current_user
+        item = query_by_id(Item, id)
+        if not item:
+            return self.write({'error':'item does not exist'})
+        feed_id = self.get_argument("feed_id", None)
+        feed = query_by_id(Feed, feed_id)
+        if not feed:
+            return self.write({'error':'feed does not exist'})
+        if feed.owner != user.wwuid and user.wwuid not in feed.administrators.split(","):
+            return self.write({'error':'insufficient permissions'})
+        name = self.get_argument("name", None)
+        start_time = self.get_argument("start_time", None)
+        end_time = self.get_argument("end_time", None)
+        user = self.current_user
+        if not name or not start_time or not end_time:
+            return self.write({'error':'you must give us a name and a start_time and an end_time'})
+        description = self.get_argument("description", None)
+
+        item.feed_id = feed_id
+        item.name = name
+        item.description = description
+        item.start_time = start_time
+        item.end_time = end_time
+        item = addOrUpdate(item)
+        self.write({'item': item.to_json()})
+
+    @tornado.web.authenticated
+    def delete(self, id):
+        user = self.current_user
+        item = query_by_id(Item, id)
+        if not item:
+            return self.write({'error':'item does not exist'})
+        feed = query_by_id(Feed, item.feed_id)
+        if not feed:
+            return self.write({'error':'feed does not exist'})
+        if feed.owner != user.wwuid and user.wwuid not in feed.administrators.split(","):
+            return self.write({'error':'insufficient permissions'})
+        delete_thing(item)
+        self.write(json.dumps("success"))
