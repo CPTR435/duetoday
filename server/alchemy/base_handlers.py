713c5c1cf1795d833ff580fa58222aaa5f537629
Brock Haugen <brock.a.haugen@gmail.com>
2015-11-16 13:31:16 -0800

    login issues

--- server/alchemy/base_handlers.py
+++ server/alchemy/base_handlers.py
@@ -14,46 +14,48 @@ class BaseHandler(tornado.web.RequestHandler):
 
     def get_current_user(self):
         wwuid = self.get_secure_cookie("wwuid")
-        user = query_user(wwuid)
+        user = query_user(str(wwuid))
         return user
 
 
 class IndexHandler(BaseHandler):
-	def get(self):
-		user = self.current_user
-		if user:
-			self.write(str(user.wwuid))
-		else:
-			self.write("Not logged in")
+    def get(self):
+        user = self.current_user
+        if user:
+            self.write(str(user.wwuid))
+        else:
+            self.write("Not logged in")
 
 
 class LoginHandler(BaseHandler):
-	def get(self):
-		self.write({'error': 'You must login to access that content'})
+    def get(self):
+        self.write({'error': 'You must login to access that content'})
 
-	def post(self):
-		logger.debug("'class':'LoginHandler','method':'post', 'message': 'invoked'")
-		username = self.get_argument('username', None)
-		password = self.get_argument('password', None)
+    def post(self):
+        logger.debug("'class':'LoginHandler','method':'post', 'message': 'invoked'")
+        username = self.get_argument('username', None)
+        password = self.get_argument('password', None)
 
-		if username and password:
-			try:
-				r = requests.post(self.application.options.auth_url, data = {'username': username, 'password': password}, verify=False)
-				o = json.loads(r.text)
-				if 'user' in o:
-					o = o['user']
-					logger.debug(o)
-					user = User(wwuid=o["wwuid"], name=o["full_name"], mask_photo=o["photo"])
-					user = addOrUpdate(user)
-					self.set_secure_cookie("wwuid", str(o['wwuid']), expires_days=30)
-					logger.info("LoginHandler: success")
-					self.write(user.to_json())
-				else:
-					logger.info("LoginHandler: error")
-					self.write({'error':'invalid login credentials'})
-			except Exception as e:
-				logger.debug("LoginHandler exception: "+ str(e.message))
-				self.write({'error': str(e.message)})
-		else:
-			logger.error("LoginHandler: invalid post parameters")
-			self.write({'error':'invalid post parameters'})
+        if username and password:
+            try:
+                r = requests.post(self.application.options.auth_url, data = {'username': username, 'password': password}, verify=False)
+                o = json.loads(r.text)
+                if 'user' in o:
+                    o = o['user']
+                    logger.debug(o)
+                    user = query_user(o["wwuid"])
+                    if not user:
+                        user = User(wwuid=o["wwuid"], name=o["full_name"], mask_photo=o["photo"])
+                        user = addOrUpdate(user)
+                    self.set_secure_cookie("wwuid", str(o['wwuid']), expires_days=30)
+                    logger.info("LoginHandler: success")
+                    self.write(user.to_json())
+                else:
+                    logger.info("LoginHandler: error")
+                    self.write({'error':'invalid login credentials'})
+            except Exception as e:
+                logger.debug("LoginHandler exception: "+ str(e.message))
+                self.write({'error': str(e.message)})
+        else:
+            logger.error("LoginHandler: invalid post parameters")
+            self.write({'error':'invalid post parameters'})
